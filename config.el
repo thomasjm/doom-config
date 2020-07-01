5ef020671c9d6f16b08dae998168bbaad269d9c8
Tom McLaughlin <pyro777@gmail.com>
2020-07-01 02:55:52 -0700

    Add before-save-hook

--- config.el
+++ config.el
@@ -223,3 +223,5 @@ import" nil t)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
