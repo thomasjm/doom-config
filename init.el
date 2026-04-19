65fc0a02754a40bfad4ef1150d3a7c7bdcbf32d5
thomasjm <tom@codedown.io>
2026-04-18 20:45:37 -0700

    init.el: enable markdown +grip

--- init.el
+++ init.el
@@ -146,7 +146,7 @@
        ;;factor
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)   ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        (nix +lsp)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
